442371b1a98b1ed50195ec4f9c18a46269cda683
omokehinde <engr.omokehinde@gmail.com>
2019-08-10 18:25:43 +0100

    build dir

---
+++ build/precache-manifest.6fea08ae0816b181f4e926c3ebc25bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16729cbd9aa5c65f6f1ba476a417f8a",
+    "url": "/mytodo/index.html"
+  },
+  {
+    "revision": "4660086537fe7294bdd9",
+    "url": "/mytodo/static/css/main.7ff73fa4.chunk.css"
+  },
+  {
+    "revision": "158ceb21e337d7808287",
+    "url": "/mytodo/static/js/2.2fb05ead.chunk.js"
+  },
+  {
+    "revision": "4660086537fe7294bdd9",
+    "url": "/mytodo/static/js/main.61f04bda.chunk.js"
+  },
+  {
+    "revision": "8c1825b80fe2a0a13d77",
+    "url": "/mytodo/static/js/runtime~main.fdc703fe.js"
+  }
+]);
